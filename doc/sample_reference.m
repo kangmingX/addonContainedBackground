18a8018739f3a27cb84d0829984a3b73966e5c09
kangmingX <76708006+kangmingX@users.noreply.github.com>
2024-09-20 15:05:54 -0400

    Add files via upload

---
+++ doc/sample_reference.m
@@ -0,0 +1,12 @@
+%% sampleFunction
+% 
+% <<sample.png>>
+% 
+% Using |sampleFunction|:
+%
+sampleFunction(42)
+
+%% With other functions
+%
+% The output of |sampleFunction| may be passed to another function:
+plot(sampleFunction(magic(4)))
