08a2c589cfe1a6d3464de300a881fba88170220e
kangmingX <76708006+kangmingX@users.noreply.github.com>
2025-01-23 13:37:35 -0500

    Add files via upload

---
+++ classes/testclass.m
@@ -0,0 +1,22 @@
+classdef testclass
+    %UNTITLED3 Summary of this class goes here
+    %   Detailed explanation goes here updated ?? updated 
+
+    properties
+        Property1
+    end
+
+    methods
+        function obj = testclass(inputArg1,inputArg2)
+            %UNTITLED3 Construct an instance of this class
+            %   Detailed explanation goes here
+            obj.Property1 = inputArg1 + inputArg2;
+        end
+
+        function outputArg = method1(obj,inputArg)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            outputArg = obj.Property1 + inputArg;
+        end
+    end
+end
